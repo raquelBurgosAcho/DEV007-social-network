38d1931216c254d87870b5e81cddba684d952d74
carolinaposadl <posadalcarolina@gmail.com>
2023-06-12 10:21:12 -0500

    Merge branch 'main' of https://github.com/raquelBurgosAcho/DEV007-social-network into home

--- src/components/login.js
+++ src/components/login.js
@@ -1,46 +1,13 @@
 export const Login = (onNavigate) => {
-  const loginDiv = document.createElement('div');
-  const titleIS = document.createElement('h2');
-  const parrafo = document.createElement('p');
-  const inputEmail = document.createElement('input');
-  const inputPassword = document.createElement('input');
-  const buttonLogin = document.createElement('button');
-  const buttongoogle = document.createElement('button');
+  const HomeDiv = document.createElement('div');
+  HomeDiv.textContent = 'Bienvenida al login';
   const buttonHome = document.createElement('button');
 
-  titleIS.textContent = ' Iniciar sesión ';
-  parrafo.textContent = ' Ingresa los datos con los que te has registrado ';
-  inputEmail.placeholder = 'Ingresa tu Email';
-  inputPassword.placeholder = 'Ingresa tu contraseña';
-  inputPassword.type = 'password';
-  buttonLogin.textContent = 'Iniciar sesión';
-  buttongoogle.textContent = 'Continuar con Google';
-  buttonHome.textContent = 'Volver a Home';
-
-  loginDiv.setAttribute('class', 'logindiv');
-  inputEmail.setAttribute('class', 'input-correo');
-  inputPassword.setAttribute('class', 'input-contraseña');
-
-  // inputEmail.setAttribute('id', 'input-email');
-  // inputPassword.setAttribute('id', 'input-password');
-
-  // const inputCorreo = loginDiv.querySelector('#input-email');
-  // const inputContraseña = loginDiv.querySelector('#input-password');*/
+  buttonHome.textContent = 'Regresar al Home';
 
   buttonHome.addEventListener('click', () => onNavigate('/'));
 
-  // buttonLogin.addEventListener('click', (e) => {
-  //   e.preventDefault();
-  //   crearUsuarioConCorreoYContraseña(inputCorreo.value, inputContraseña.value);
-  // });
+  HomeDiv.appendChild(buttonHome);
 
-  loginDiv.appendChild(titleIS);
-  loginDiv.appendChild(parrafo);
-  loginDiv.appendChild(inputEmail);
-  loginDiv.appendChild(inputPassword);
-  loginDiv.appendChild(buttonLogin);
-  loginDiv.appendChild(buttongoogle);
-  loginDiv.appendChild(buttonHome);
-
-  return loginDiv;
+  return HomeDiv;
 };
