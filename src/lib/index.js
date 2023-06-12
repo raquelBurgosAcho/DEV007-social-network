2688f83aca80f93ece5a2e8e658fb3a612d524bf
carolinaposadl <posadalcarolina@gmail.com>
2023-06-12 10:13:57 -0500

    archivos actualizados

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,16 +1,15 @@
-import { createUserWithEmailAndPassword } from 'firebase/auth';
-import { auth, db } from '../firebase';
-import { addDoc, collection } from 'firebase/firestore';
+// import { createUserWithEmailAndPassword } from 'firebase/auth';
+// import { auth, db } from '../firebase';
+// import { addDoc, collection } from 'firebase/firestore';
 
-export const crearUsuarioConCorreoYContraseña = (email, contraseña) => {
-  createUserWithEmailAndPassword(auth, email, contraseña);
-};
+// export const crearUsuarioConCorreoYContraseña = (email, contraseña) => {
+//   createUserWithEmailAndPassword(auth, email, contraseña);
+// };
 
-export const crearPost = (text) => {
-  addDoc(collection(db, 'posts'), {
-    contenido: text,
-  },
+// export const crearPost = (text) => {
+//   addDoc(collection(db, 'posts'), {
+//     contenido: text,
+//   },
 
-  ),
-};
-
+//   ),
+// },
