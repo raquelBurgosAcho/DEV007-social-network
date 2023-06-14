d7ef0d3b4edada883d2ff51d509d73800c2208f2
Ana Laura Vargas Piña <analauvapi15@gmail.com>
2023-06-14 11:21:28 -0600

    avanza las validaciones de google y correo electronico

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,37 +1,17 @@
 import { addDoc, collection } from 'firebase/firestore';
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signInWithPopup } from 'firebase/auth';
-import { auth, db } from '../firebase';
+import { auth, db, provider } from '../firebase';
 
 export const crearUsuarioConCorreoYContraseña = (email, contraseña) => {
-  createUserWithEmailAndPassword(auth, email, contraseña);
+  return createUserWithEmailAndPassword(auth, email, contraseña);
 };
 
 export const iniciarSesionConUsuarioYContraseña = (email, contraseña) => {
-  signInWithEmailAndPassword(auth, email, contraseña);
+  return signInWithEmailAndPassword(auth, email, contraseña);
 };
 
-export const iniciarSesionConGoogle = (email, contraseña) => {
-  signInWithPopup(auth, email, contraseña);
-  const auth = getAuth();
-  signInWithPopup(auth, provider)
-    .then((result) => {
-      // This gives you a Google Access Token. You can use it to access the Google API.
-      const credential = GoogleAuthProvider.credentialFromResult(result);
-      const token = credential.accessToken;
-      // The signed-in user info.
-      const user = result.user;
-      // IdP data available using getAdditionalUserInfo(result)
-      // ...
-    }).catch((error) => {
-      // Handle Errors here.
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      // The email of the user's account used.
-      const email = error.customData.email;
-      // The AuthCredential type that was used.
-      const credential = GoogleAuthProvider.credentialFromError(error);
-      // ...
-    });
+export const iniciarSesionConGoogle = () => {
+  return signInWithPopup(auth, provider);
 };
 
 export const crearPost = (text) => {
