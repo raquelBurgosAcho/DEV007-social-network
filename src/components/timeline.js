d762074bbdfbb5d847b4e93185b143d9e6d0a503
carolinaposadl <posadalcarolina@gmail.com>
2023-06-20 18:22:02 -0500

    prueba de código con innerHTML

--- src/components/timeline.js
+++ src/components/timeline.js
@@ -87,11 +87,167 @@ export const Timeline = (onNavigate) => {
   return postDiv;
 };
 
-//  Condicional de para limpiar contenedor y asegurarse de que el usuario deba ingresar algo
 
-// if (contenidoPost === '') {
-//   errorTextoVacio.textContent = 'Por favor ingresa tu comentario';
-//   errorTextoVacio.style.display = 'block';
-// } else {
-//     errorTextoVacio.style.display = 'none';
-//     articlePost.querySelector('#inp-post').value = ''; // Limpiar el área de texto
+
+//////////////////////////////////
+
+import {
+  crearPost,
+  guardarTodosLosPost,
+  eliminarPost,
+  toDislike,
+  toLike,
+} from '../lib';
+
+export const Timeline = (onNavigate, user) => {
+  // Div que almacena todo-------------------------------------
+  const postDiv = document.createElement('div');
+  postDiv.className = 'login-register-div ';
+
+  // titulo ------------------------------------------------------
+  const titleFloraTimeline = document.createElement('header');
+  titleFloraTimeline.textContent = 'Flora';
+  titleFloraTimeline.className = 'title-flora';
+
+  // almacena el text AREA ------------------------------------------------------
+  const articlePost = document.createElement('article');
+  articlePost.className = 'login-register-div ';
+
+  // NOMBRE DE USUARIO ------------------------------------------------------
+  const nameUser = document.createElement('h5');
+  nameUser.className = 'logo-flora';
+
+  // TEXT AREA -------------------------------------------------------------
+  const textArea = document.createElement('textarea');
+  textArea.name = 'textarea';
+  textArea.rows = '10';
+  textArea.cols = '50';
+  textArea.className = 'inpPost';
+  textArea.id = 'inpPost';
+  textArea.placeholder = 'Escribe aquí...';
+
+  // BOTON PUBLICAR ------------------------------------------------------
+  const newPost = document.createElement('button');
+  newPost.className = 'button';
+  newPost.id = 'newPost';
+  newPost.textContent = 'Publicar';
+
+  // ERROR DE CAMPO VACIO ANTES DE PUBLICAR ------------------------------------------------------
+  const errorTextoVacio = document.createElement('h4');
+  errorTextoVacio.textContent = '';
+  errorTextoVacio.setAttribute('class', 'error-message');
+
+  // DIVS DE POST REALIZADOS ------------------------------------------------------
+  const postsContainer = document.createElement('div');
+  postsContainer.className = 'posts-container';
+
+  // VOLVER A HOME ------------------------------------------------------
+  const buttonHome = document.createElement('button');
+  buttonHome.textContent = 'Cerrar sesión';
+
+  postDiv.appendChild(titleFloraTimeline);
+  // articlePost.appendChild(nameUser);
+  articlePost.appendChild(textArea);
+  articlePost.appendChild(newPost);
+  postDiv.appendChild(articlePost);
+  articlePost.appendChild(errorTextoVacio);
+  articlePost.appendChild(postsContainer);
+  postDiv.appendChild(buttonHome);
+
+  // EVENTO BOTON IR A  HOME ------------------------------------------------------
+  buttonHome.addEventListener('click', () => onNavigate('/'));
+
+  // ---------------------BOTON PUBLICAR---------------------------------------------------------
+
+  newPost.addEventListener('click', () => {
+    const contenidoPost = textArea.value;
+
+    if (contenidoPost === '') {
+      errorTextoVacio.textContent = 'Por favor ingresa tu comentario.';
+      errorTextoVacio.style.display = 'block';
+    } else {
+      errorTextoVacio.style.display = 'none';
+      textArea.value = '';
+
+      crearPost(contenidoPost)
+        .then(() => guardarTodosLosPost())
+        .then((posts) => {
+          postsContainer.innerHTML = ''; // Limpiar el contenedor de publicaciones antes de generar los nuevos elementos
+
+          posts.forEach((post) => {
+            const postElement = document.createElement('div');
+            postElement.className = 'divPost';
+
+            const article = document.createElement('article');
+            article.className = 'articlePost';
+            article.id = 'articlePost';
+
+            const contenidoElement = document.createElement('p');
+            contenidoElement.textContent = post.contenido;
+
+            const bottomDiv = document.createElement('div');
+            bottomDiv.className = 'bottomDiv';
+
+            const btnsLike = document.createElement('button');
+            btnsLike.className = 'btnLike';
+            btnsLike.setAttribute('btnLikes', post.id);
+            btnsLike.id = 'btnsLikes';
+
+            const like = document.createElement('img');
+            like.className = 'like';
+            like.src = './images/heart.png';
+
+            const dislike = document.createElement('img');
+            dislike.className = 'dislike';
+            dislike.src = './images/full-heart.png';
+            dislike.style.display = 'none';
+
+            const btnsLikes = postsContainer.querySelectorAll('#btnLikes');
+            btnsLikes.forEach((btn) => {
+              btn.addEventListener('click', async () => {
+                const getIdPost = btn.getAttribute('btnLikes');
+                if (getIdPost === post.id) {
+                  const document = await guardarTodosLosPost(posts.id);
+                  const postear = document.data();
+                  if (postear.likes.includes(user.uid)) {
+                    toDislike(post.id, user.uid);
+                  } else {
+                    toLike(post.id, user.uid);
+                  }
+                }
+              });
+            });
+
+            const botonEliminar = document.createElement('button');
+            botonEliminar.className = 'btnDelete';
+            botonEliminar.textContent = 'Eliminar';
+            botonEliminar.addEventListener('click', () => {
+              eliminarPost(post.id)
+                .then(() => {
+                  postElement.remove(); // Eliminar el elemento del DOM después de eliminar el post
+                })
+                .catch((error) => {
+                  console.log('Error al eliminar el post:', error);
+                });
+            });
+
+            bottomDiv.appendChild(btnsLike);
+            btnsLike.appendChild(like);
+            btnsLike.appendChild(dislike);
+            bottomDiv.appendChild(botonEliminar);
+
+            article.appendChild(contenidoElement);
+            article.appendChild(bottomDiv);
+
+            postElement.appendChild(article);
+            postsContainer.appendChild(postElement);
+          });
+        })
+        .catch((error) => {
+          console.log('Error al crear el post:', error);
+        });
+    }
+  });
+
+  return postDiv;
+};
--- src/lib/index.js
+++ src/lib/index.js
@@ -1,5 +1,5 @@
 // En este archivo están todas las funciones principales del proyecto
-import { addDoc, collection, onSnapshot } from 'firebase/firestore';
+import { addDoc, collection, getDocs, deleteDoc, doc, updateDoc, arrayUnion, arrayRemove } from 'firebase/firestore';
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signInWithPopup } from 'firebase/auth';
 import { auth, db, provider } from '../firebase';
 
@@ -11,8 +11,8 @@ export const iniciarSesionConUsuarioYContraseña = async (email, contraseña) =>
   await signInWithEmailAndPassword(auth, email, contraseña);
 };
 
-export const iniciarSesionConGoogle = () => {
-  signInWithPopup(auth, provider);
+export const iniciarSesionConGoogle = async () => {
+  await signInWithPopup(auth, provider);
 };
 
 export const crearPost = async (texto) => {
@@ -21,6 +21,30 @@ export const crearPost = async (texto) => {
   });
 };
 
-export const guardarTodosLosPost = () => {
-  onSnapshot(collection(db, 'posts'));
+export const guardarTodosLosPost = async () => {
+  // realizar consulta a la coleccion y alamcenarla en snapshot
+  const snapshot = await getDocs(collection(db, 'posts'));
+  const posts = snapshot.docs.map((doc) => ({
+    // iterar sobre cada documento y extraer el contenido de cada uno
+    contenido: doc.data().contenido,
+    // y el id elimina cada comentario
+    id: doc.id,
+  }));
+  return posts;
+};
+
+export const eliminarPost = async (id) => {
+  await deleteDoc(doc(db, 'posts', id));
+};
+
+export const toLike = (id, uid) => {
+  updateDoc(doc(db, 'posts', id), {
+    likes: arrayUnion(uid),
+  });
+};
+
+export const toDislike = (id, uid) => {
+  updateDoc(doc(db, 'posts', id), {
+    likes: arrayRemove(uid),
+  });
 };
